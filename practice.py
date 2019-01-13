eccb97ee0b9350bd205a28725b2d9371b70bba3e
Jennifer Bergeson <jberges@purdue.edu>
2019-01-13 14:52:42 -0500

    Multiple plots

--- practice.py
+++ practice.py
@@ -1,6 +1,24 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+# x = np.linspace(0, 2*np.pi, 500)
+# y = np.sin(x)
+# plt.plot(y)
+# plt.show()
+
+fig, axes = plt.subplots(nrows=4)
+
+def sine_function(idx,input):
+    y = np.sin(input)
+    axes[idx].plot(input, y)
+
 x = np.linspace(0, 2*np.pi, 500)
-y = np.sin(x)
-plt.plot(y)
+a = np.linspace(0, 3*np.pi, 500)
+b = np.linspace(0, 4*np.pi, 500)
+c = np.linspace(0, 5*np.pi, 500)
+
+sine_function(0,x)
+sine_function(1,a)
+sine_function(2,b)
+sine_function(3,c)
+
 plt.show()
