e97481c2267d1969f90f863f7fc31b1c32df9d8b
Jennifer Bergeson <jberges@purdue.edu>
2019-01-13 18:37:33 -0500

    Fun With Strings

--- Hello.py
+++ Hello.py
@@ -8,18 +8,47 @@
 # b = "guru99"
 # print(b)
 # print(str(a) + b)
-f = 101
-print(f)
-def my_func():
-    f = "I am learning Python"
-    print(f)
-my_func()
-print(f)
-def newVar():
-    global f
-    print(f)
-    f = "new f"
-newVar()
-print(f)
-del f
-print(f)
+# f = 101
+# print(f)
+# def my_func():
+#     f = "I am learning Python"
+#     print(f)
+# my_func()
+# print(f)
+# def newVar():
+#     global f
+#     print(f)
+#     f = "new f"
+# newVar()
+# print(f)
+# del f
+# var1 = "Guru99"
+# var2 = "Software Testing"
+# print(var1[0])
+# print(var2[1:5])
+# print(var2[:])
+# print(var1 * 2)
+# x = "u" not in var1
+# print(x)
+# print("hi"r"\nbye")
+# name = "guru"
+# number = 99
+# print("%s %d" % (name, number))
+# print(str(number)*5)
+# x = "Hello World!"
+# print(x[:6])
+# print(x[0:6] + "Guru99")
+# print(x[0:6])
+# oldstring = "I like chocolate"
+# newstring = oldstring.replace("like", "love")
+# print(newstring)
+# print(oldstring)
+# print(newstring.upper())
+# print(oldstring.lower())
+# # print(":".join("Python"))
+# var = "12345"
+# print("".join(reversed(var)))
+# mystring = "dword1 dword2 dword3"
+# print(mystring.split("d"))
+
+
