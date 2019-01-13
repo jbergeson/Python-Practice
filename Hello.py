56bc1004e7d7df1e070f221cedd48e5254db220d
Jennifer Bergeson <jberges@purdue.edu>
2019-01-13 17:38:36 -0500

    Variable Practice

--- Hello.py
+++ Hello.py
@@ -1,1 +1,25 @@
-print("Hello World")
+#print("Hello World")
+# def main():
+#     print("Hello world!")
+# print("Guru99")
+# main()
+# a = 9
+# print(a)
+# b = "guru99"
+# print(b)
+# print(str(a) + b)
+f = 101
+print(f)
+def my_func():
+    f = "I am learning Python"
+    print(f)
+my_func()
+print(f)
+def newVar():
+    global f
+    print(f)
+    f = "new f"
+newVar()
+print(f)
+del f
+print(f)
